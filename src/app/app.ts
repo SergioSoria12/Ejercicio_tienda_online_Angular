831c26d3b8d4cdf81c0cab517a87ccb1cb0eb3a4
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-08-12 11:18:22 +0200

    Terminado Ejercicio sencillo de manejo componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListadoProductos } from "./listado-productos/listado-productos";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListadoProductos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('tienda-online');
+  title = 'Tienda Online';
 }
