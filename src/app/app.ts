6ffb73142169dbb990301cf5223167f6163e5d09
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-08-31 12:48:06 +0200

    Deploy de web-app con hosting de firebase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component} from '@angular/core';
-import { ListadoProductos } from "./listado-productos/listado-productos";
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LoginService } from './login.service';
 
@@ -7,7 +6,7 @@ import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
-  imports: [ListadoProductos, RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
